02e4aeaeb5eab9f13276c6166c5c3d2e27f33b88
Dixin Fan <dishan@mail.ustc.edu.cn>
2021-12-09 18:13:40 +0800

    add function caculate_height

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import triangulation as tri
 import calibration
 # Mediapipe for face detection
 import mediapipe as mp
-import calibration_depth as cd
 
 
 mp_facedetector = mp.solutions.face_detection
@@ -19,8 +18,42 @@ f = 2.37              #Camera lense's focal length [mm]
 alpha = 88        #Camera field of view in the horisontal plane [degrees]
 
 
+def calibrate_depth(depth):
+    # (51：19) (83：22)
+    slope = 11
+    bias = -159
+    depth = slope * depth + bias
+    depth = round(depth,  0)
+    # ones_place = depth % 10
+    # print('test')
+    # print(depth)
+    # print(ones_place)
+    # if ones_place in range(1 , 3):
+    #     depth =  depth - ones_place
+    # elif ones_place in range(7, 10):
+    #     depth =  depth - ones_place + 10
+    # depth = round(depth,  -1)
+    # print(depth)
+    return depth
+
+def caculate_height(depth, scale_h_left):
+    """
+    h/d     51      83
+    180     0.31    0.41
+    175     0.36    0.46
+    170     0.46    0.52
+    165     0.5     0.54
+    """
+    slope_1 = 0.003
+    slope_2 = -0.01
+    bias = 1.957
+    height = (scale_h_left - slope_1*depth - bias) / slope_2
+    return height
+
+
 # Main program loop with face detector and depth estimation using stereo vision
 with mp_facedetector.FaceDetection(min_detection_confidence=0.7) as face_detection:
+    scale_h_left = 0
     while(cap_right.isOpened() and cap_left.isOpened()):
         succes_right, frame_right = cap_right.read()
         succes_left, frame_left = cap_left.read()
@@ -55,8 +88,19 @@ with mp_facedetector.FaceDetection(min_detection_confidence=0.7) as face_detecti
                     mp_draw.draw_detection(frame_left, detection)
                     bBox = detection.location_data.relative_bounding_box
                     h, w, c = frame_left.shape
+                    # print(bBox)
+                    # print(h)
+                    # print('test')
+                    # print(bBox.ymin)
+                    # print(bBox.height)
+                    # print(bBox.height / 2)
+                    # scale_h = bBox.ymin + bBox.height / 2
+                    # print(round(scale_h, 2))
+                    # print(bBox.ymin + bBox.height / 2)
+                    scale_h_left = bBox.ymin + bBox.height / 2
                     boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
                     center_point_left = (boundBox[0] + boundBox[2] / 2, boundBox[1] + boundBox[3] / 2)
+                    # print(boundBox)
                     cv2.putText(frame_left, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
             # If no ball can be caught in one camera show text "TRACKING LOST"
             if not results_right.detections or not results_left.detections:
@@ -65,10 +109,15 @@ with mp_facedetector.FaceDetection(min_detection_confidence=0.7) as face_detecti
             else:
                 # Function to calculate depth of object. Outputs vector of all depths in case of several balls.
                 # All formulas used to find depth is in video presentaion
+                # print(center_point_right)
+                # print("陈")
+                # print(center_point_left)
                 depth = tri.find_depth(center_point_right, center_point_left, frame_right, frame_left, B, f, alpha)
-                depth = cd.calibrate_depth(depth)
+                depth = calibrate_depth(depth)
+                height = caculate_height(depth, scale_h_left)
                 cv2.putText(frame_right, "Distance: " + str(round(depth,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
-                cv2.putText(frame_left, "Distance: " + str(round(depth,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
+                cv2.putText(frame_left, "Height: " + str(round(height,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
+                # cv2.putText(frame_left, "Distance: " + str(round(depth,1)), (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0,255,0),3)
                 # Multiply computer value with 205.8 to get real-life depth in [cm]. The factor was found manually.
             # Show the frames
             cv2.imshow("frame right", frame_right) 
